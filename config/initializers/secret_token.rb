64106c8c20f881709dbd0c9d9cbe40b58c46f2f3
vesselinv <vesselinv@me.com>
2014-03-01 15:56:47 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthMigrate::Application.config.secret_key_base = '28661a5e256693ae2934bf891af829829e1481f20c55d63bafb2ef3610fed7676d538dc2ffc4505d6ddc539b2bc7cd635c3a90baedca9ee9b54c52e4940d2e6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuthMigrate::Application.config.session_store :cookie_store, key: '_auth-migrate_session'
